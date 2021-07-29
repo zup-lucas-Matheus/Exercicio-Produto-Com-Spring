6e3e99f4c5a8268a5d07df3ef1568412e98ef707
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-07-29 14:37:40 -0300

    Commit minha classe controllador com meus metódos.

--- src/main/java/br/com/zup/ListaDeProdutos/controllers/ProdutoController.java
+++ src/main/java/br/com/zup/ListaDeProdutos/controllers/ProdutoController.java
@@ -1,19 +1,33 @@
 package br.com.zup.ListaDeProdutos.controllers;
 
+import br.com.zup.ListaDeProdutos.dominio.Produto;
 import br.com.zup.ListaDeProdutos.service.ProdutoService;
+import org.apache.catalina.LifecycleState;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController // meu controlador
+import java.util.List;
+
+@RestController// meu controlador
+@RequestMapping("/produtos") // Fazendo um mapeamento generico para todos os meu metódos abaixo.
 public class ProdutoController {
 
     @Autowired // o spring faz a injeção do meu produtoService.
     private ProdutoService produtoService;
 
+    @PostMapping //Vou receber informação nesse metódo...
+    public List<Produto> cadastrarProduto(@RequestBody Produto produto){
 
+        produtoService.adicionarProduto(produto);
+        return produtoService.retornarEstoque();
 
+    }
 
+    @GetMapping // Retornar a informação, não preciso receber...
+    public List<Produto> visualizarListaDeProdutos(){
 
+        return produtoService.retornarEstoque();
 
+    }
 
 }
